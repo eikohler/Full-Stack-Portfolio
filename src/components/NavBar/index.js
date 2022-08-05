fc8d5ae4e26fe3337b5ece6190a6d9634752bc0d
Eric Kohler <erickohler1600@gmail.com>
2022-08-05 01:51:08 -0400

    setup dark mode switch

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        if(isDarkMode){
+            document.documentElement.setAttribute('data-theme', 'light');
+        }else{
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        setIsDarkMode(!isDarkMode);
+    }
+
   return (
     <header className='d-flex flex-column justify-content-between h-100'>        
         <nav>
@@ -11,7 +22,7 @@ function NavBar() {
             <span>work</span>
             <span>contact</span>
         </nav>  
-        <span>
+        <span onClick={() => toggleDarkMode()}>
             <FontAwesomeIcon icon={faMoon} size="2x" />
         </span>
     </header>
