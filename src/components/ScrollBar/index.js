26953af357fec93069222346b62aaeb337d66ca6
Eric Kohler <erickohler1600@gmail.com>
2022-08-10 14:24:30 -0400

    btn scrolls with page

--- src/components/ScrollBar/index.js
+++ src/components/ScrollBar/index.js
@@ -6,17 +6,20 @@ class ScrollBar extends Component {
         super();
         this.totalHeight = 0;
         this.trackHeight = 0;
+        this.btnHeight = 0;
         this.percentDiff = 0;
         this.updateHeight = this.updateHeight.bind(this);
-      }
+    }
 
-    updateHeight(){        
-        $('.scroll-btn').css('top', `${window.scrollY}px`);
+    updateHeight(){
+        // console.log(window.scrollY)
+        $('.scroll-btn').css('top', `${window.scrollY*this.percentDiff}px`);
     }
 
     componentDidMount(){
-        this.totalHeight = $(document).height();
-        this.trackHeight = $('.scroll-track').height();
+        this.totalHeight = (document.body.offsetHeight-window.innerHeight)+100;
+        this.btnHeight = document.querySelector('.scroll-btn').clientHeight;
+        this.trackHeight = document.querySelector('.scroll-track').clientHeight - this.btnHeight;
         this.percentDiff = this.trackHeight / this.totalHeight;
 
         window.addEventListener('scroll', this.updateHeight);        
