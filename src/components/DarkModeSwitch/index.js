771a4276c0b7a230c1dd7f36f6463ad20207d29d
Eric Kohler <erickohler1600@gmail.com>
2022-08-10 08:49:19 -0400

    update header styling, color styling, nav effect

--- src/components/DarkModeSwitch/index.js
+++ src/components/DarkModeSwitch/index.js
@@ -5,28 +5,28 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 function DarkModeSwitch() {
     const [showDarkMode, setShowDarkMode] = useState(false);
 
-    useEffect(() => {
-        const data = localStorage.getItem('dark_mode_theme');
-        console.log(data);
-        if(data) {
-            setShowDarkMode(data);
-        }   
-    }, []);
+    // useEffect(() => {
+    //     const data = localStorage.getItem('dark_mode_theme');
+    //     console.log(data);
+    //     if(data) {
+    //         setShowDarkMode(data);
+    //     }   
+    // }, []);
 
     useEffect(() => {
         console.log(showDarkMode);
-        localStorage.setItem('dark_mode_theme', showDarkMode);
+        // localStorage.setItem('dark_mode_theme', showDarkMode);
         if(showDarkMode){
             console.log("darkmode");
-            // document.documentElement.setAttribute('data-theme', 'dark');
+            document.documentElement.setAttribute('data-theme', 'dark');
         }else{
             console.log("lightmode");
-            // document.documentElement.setAttribute('data-theme', 'light');
+            document.documentElement.setAttribute('data-theme', 'light');
         }
     }, [showDarkMode]);
 
   return (
-    <span className='mode-switch' onClick={() => {console.log("test"); setShowDarkMode(!showDarkMode)}}>
+    <span className='mode-switch' onClick={()=>setShowDarkMode(!showDarkMode)}>
         {showDarkMode ? <FontAwesomeIcon icon={faSun} size="2x" />
         : <FontAwesomeIcon icon={faMoon} size="2x" />}
     </span>
