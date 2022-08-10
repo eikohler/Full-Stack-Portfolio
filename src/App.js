d997fe045dde2d581d99abf0c25a81ddf2901f1e
Eric Kohler <erickohler1600@gmail.com>
2022-08-10 09:53:08 -0400

    reorder app layout columns

--- src/App.js
+++ src/App.js
@@ -6,16 +6,27 @@ import Home from './components/Home';
 function App() {
   return (
     <>
-      <div className='main-container container mt-5'>
+      <div className='container my-5'>
         <section className='row justify-content-between'>
           <div className='col-1'>
             <Navbar />
           </div>
           <div className='col-10'>
-            <main>
-              <Home />
-            </main>
-          </div>
+            <section className='row'>
+                <div className='col-11'>
+                  <main>
+                    <Home />
+                    <Home />
+                    <Home />
+                    <Home />
+                    <div className='bottom-spacer'></div>
+                  </main>
+                </div>
+                <div className='col-1'>
+
+                </div>
+            </section>            
+          </div>        
         </section>
       </div>
       <Footer />
