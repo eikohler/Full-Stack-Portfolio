de82be54bbafd8ef821a205aa0ad7cdda962d31b
Eric Kohler <erickohler1600@gmail.com>
2022-08-10 12:29:05 -0400

    remove useOnScreen hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import './App.css';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
@@ -6,8 +6,6 @@ import Home from './components/Home';
 
 
 function App() {
-  const ref = useRef(null);
-
   return (
     <>
       <div className='container my-5'>
@@ -19,16 +17,16 @@ function App() {
             <section className='row'>
                 <div className='col-11'>
                   <main>
-                    <section className="section-container" ref={ref}>
+                    <section className="section-container">
                       <Home />
                     </section>
-                    <section className="section-container" ref={ref}>
+                    <section className="section-container">
                       <Home />
                     </section>
-                    <section className="section-container" ref={ref}>
+                    <section className="section-container">
                       <Home />
                     </section>
-                    <section className="section-container" ref={ref}>
+                    <section className="section-container">
                       <Home />
                     </section>
                     <div className='bottom-spacer'></div>
