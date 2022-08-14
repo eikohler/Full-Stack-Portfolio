d08f042a6e427d72e18c491fc3793548e6595daf
Eric Kohler <erickohler1600@gmail.com>
2022-08-14 10:50:23 -0400

    setup modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Modal = ({ onClose, currentWork }) => {
+    const {image, name, tools, app, github, summaryPoints} = currentWork;
+    return (
+        <div className='modal-container'>
+
+        </div>
+  );
+}
+
+export default Modal;
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
 import db from '../../assets/json/work.json';
+import Modal from '../Modal';
 
 function Work() {
   const [works] = useState(db.works);
+  const [currentWork, setCurrentWork] = useState();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = (work) => {
+    setCurrentWork(work);
+    setModalOpen(!modalOpen);
+  }
     
   return (
-    <section className="row">
-      {works.map((work) => (
-        <div className='col-md-4 p-2' key={work.name}>
-          <div className='gallery-image-container'>
-            <h3>{work.name}</h3>
-            <div className='overlay'></div>
-            <img
-              src={require(`../../assets/gifs/${work.image}`)}
-              alt={work.name}
-              className="gallery-image"
-              // onClick={() => toggleModal(work)}
-              key={work.name}
-            />
+    <>
+      {modalOpen && (
+        <Modal currentWork={currentWork} onClose={toggleModal} />
+      )}
+      <section className="row">
+        {works.map((work) => (
+          <div className='col-md-4 p-2' key={work.name}>
+            <div className='gallery-image-container'>
+              <h3>{work.name}</h3>
+              <div className='overlay'></div>
+              <img
+                src={require(`../../assets/gifs/${work.image}`)}
+                alt={work.name}
+                className="gallery-image"
+                onClick={() => toggleModal(work)}
+                key={work.name}
+              />
+            </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+    </>
   );
 }
 
