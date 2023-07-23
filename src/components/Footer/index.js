44e9cab781c4892cf1cf43b9ecaa493a12574417
Eric Kohler <erickohler1600@gmail.com>
2023-07-23 14:00:22 -0700

    update links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,8 @@ function Footer() {
         <div className='container-fluid'>
           <section className='row mx-0 justify-content-center'>
             <div className='col-xl-10 col-lg-11 px-0 d-flex justify-content-sm-around justify-content-between'>
-              <a href="mailto:erickohler1600@gmail.com">email</a>
-              <a href="https://www.linkedin.com/in/eric-kohler-379592204/" target="_blank" rel="noopener noreferrer">linkedin</a>
+              <a href="mailto:erickohlerdev@gmail.com">contact</a>
+              <a href="https://www.linkedin.com/in/erickohlerdev" target="_blank" rel="noopener noreferrer">linkedin</a>
               <a href="https://github.com/eikohler" target="_blank" rel="noopener noreferrer">github</a>
             </div>
           </section>
