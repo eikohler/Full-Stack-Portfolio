94da241a100c1eaf4370ead7af7828b830e68d69
Eric Kohler <erickohler1600@gmail.com>
2023-07-24 23:03:49 -0700

    added animation for about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const variants = {
+  visible: { opacity: 1, y: "0px", transition: { duration: 0.5 } },
+  hidden: { opacity: 0, y: "100px" }
+};
 
 function About() {
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+    controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
     <div className="row mx-0 justify-content-center">        
       <div className="col-lg-7 col-sm-10 text-center">
-        <p><span>Hello!</span> I'm a Vancouver-based Front-End Developer specializing in 
-        building and growing eCommerce platforms with advanced skills in HTML, CSS, JavaScript, 
-        jQuery, Bootstrap, React and PHP.</p>
-        <p>My background is in Computer Science and Visual Arts from Brock University. 
-          As well, I've completed the Full Stack Coding BootCamp from Carleton University 
-          and further went on to assist in teaching the same course at the University of Richmond.
-          I have 2+ years experience working alongside designers 
-          and developers to bring client's ideas and design concepts to life.</p>        
-        <p>See my past work projects <a href="#work">below.</a></p>
+        <motion.div ref={ref} variants={variants} initial="hidden" animate={controls}>
+          <p><span>Hello!</span> I'm a Vancouver-based Front-End Developer specializing in 
+          building and growing eCommerce platforms with advanced skills in HTML, CSS, JavaScript, 
+          jQuery, Bootstrap, React and PHP.</p>
+          <p>My background is in Computer Science and Visual Arts from Brock University. 
+            As well, I've completed the Full Stack Coding BootCamp from Carleton University 
+            and further went on to assist in teaching the same course at the University of Richmond.
+            I have 2+ years experience working alongside designers 
+            and developers to bring client's ideas and design concepts to life.</p>        
+          <p>See my past work projects <a href="#work">below.</a></p>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/WorkItem/index.js
+++ src/components/WorkItem/index.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
-const workVariants = {
-  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-  hidden: { opacity: 0, scale: 0 }
+const variants = {
+    visible: { opacity: 1, y: "0px", transition: { duration: 0.5 } },
+    hidden: { opacity: 0, y: "100px" }
 };
 
 function WorkItem(props) {
@@ -20,7 +20,7 @@ function WorkItem(props) {
     }, [controls, inView]);
   
     return (    
-        <motion.div ref={ref} variants={workVariants} initial="hidden" animate={controls}>
+        <motion.div ref={ref} variants={variants} initial="hidden" animate={controls}>
             <a target="_blank" rel="noopener noreferrer" href={`${work.url}`}
             className='no-highlight' key={i+" "+work.name+" container"}>        
                 <div className="mb-4 px-lg-4 work-image-wrapper">
