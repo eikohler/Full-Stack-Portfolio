9a1e26cd1b3e6d7a5af949464c21d1282e715c12
Eric Kohler <erickohler1600@gmail.com>
2023-09-21 21:25:35 -0700

    updated text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,14 +22,15 @@ function About() {
     <div className="row mx-0 justify-content-center">        
       <div className="col-lg-7 col-sm-10 text-center">
         <motion.div ref={ref} variants={variants} initial="hidden" animate={controls}>
-          <p><span>Hello!</span> I'm a Vancouver-based Front-End Developer specializing in 
-          building and growing eCommerce platforms with advanced skills in HTML, CSS, JavaScript, 
-          jQuery, Bootstrap, React and PHP.</p>
+          <p><span>Hello!</span> I'm a Vancouver-based Full-Stack Web Developer specializing in 
+          Front End Development with advanced skills in HTML, CSS, JavaScript, jQuery, Bootstrap, React and PHP.<br></br>
+          I also specialize in building eCommerce and user-friendly websites through WordPress and Shopify.</p>
           <p>My background is in Computer Science and Visual Arts from Brock University. 
-            As well, I've completed the Full Stack Coding BootCamp from Carleton University 
-            and further went on to assist in teaching the same course at the University of Richmond.
-            I have 2+ years experience working alongside designers 
-            and developers to bring client's ideas and design concepts to life.</p>        
+            As well, I've completed the Full-Stack Coding BootCamp from Carleton University 
+            and further went on to assist in teaching the same course at the University of Richmond. 
+            I also have a strong interest in UI/UX design.</p>
+          <p>I have 2+ years experience working alongside designers and developers to bring client's 
+            ideas and design concepts to life.</p>
           <p>See my past work projects <a href="#work">below.</a></p>
         </motion.div>
       </div>
