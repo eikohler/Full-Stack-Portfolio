62db5a4569b3ceb90097abf3d2d46b51c6fa0e26
Eric Kohler <erickohler1600@gmail.com>
2023-09-21 21:33:00 -0700

    update css font sizes and update text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,7 +22,7 @@ function About() {
     <div className="row mx-0 justify-content-center">        
       <div className="col-lg-7 col-sm-10 text-center">
         <motion.div ref={ref} variants={variants} initial="hidden" animate={controls}>
-          <p><span>Hello!</span> I'm a Vancouver-based Full-Stack Web Developer specializing in 
+          <p><span>Hello!</span> I'm a Vancouver-based Full-Stack Developer specializing in 
           Front End Development with advanced skills in HTML, CSS, JavaScript, jQuery, Bootstrap, React and PHP.<br></br>
           I also specialize in building eCommerce and user-friendly websites through WordPress and Shopify.</p>
           <p>My background is in Computer Science and Visual Arts from Brock University. 
